c0fad0a829a94954d46dc9267d08b6a458a3a775
Aaron Parecki <aaron@parecki.com>
2013-05-07 14:45:46 -0700

    remove png from marker list

--- files.php
+++ files.php
@@ -2,9 +2,9 @@
 
 $files = glob('images/*.png');
 foreach($files as $f) {
-  preg_match('/images\/(.+)/', $f, $match);
-  if($match[1] == 'powered-by-esri.png')
+  preg_match('/images\/(.+)\.png/', $f, $match);
+  if($match[1] == 'powered-by-esri')
     continue;
-  
-  echo '* ![' . $match[1] . '](https://devtopia/Portland-R-D-Center/static-maps-api/raw/master/images/' . $match[1] . ') `' . $match[1] . '`' . "\n";
+
+  echo '* ![' . $match[1] . '](https://devtopia/Portland-R-D-Center/static-maps-api/raw/master/images/' . $match[1] . '.png) `' . $match[1] . '`' . "\n";
 }
