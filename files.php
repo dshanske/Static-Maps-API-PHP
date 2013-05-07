6a27267f3ca56a3c9a3e39cf562b8b569b136862
Aaron Parecki <aaron@parecki.com>
2013-05-07 14:44:05 -0700

    fix marker list

--- files.php
+++ files.php
@@ -3,5 +3,8 @@
 $files = glob('images/*.png');
 foreach($files as $f) {
   preg_match('/images\/(.+)/', $f, $match);
-  echo '* `' . $match[1] . '` ![' . $match[1] . '](https://devtopia/Portland-R-D-Center/static-maps-api/raw/master/images/' . $match[1] . ')' . "\n";
+  if($match[1] == 'powered-by-esri.png')
+    continue;
+  
+  echo '* ![' . $match[1] . '](https://devtopia/Portland-R-D-Center/static-maps-api/raw/master/images/' . $match[1] . ') `' . $match[1] . '`' . "\n";
 }
